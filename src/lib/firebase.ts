b384de5efa3fd436883473b58b9b5d2eab13fd49
Wahyu Cahyo <wahyucahyo982@gmail.com>
2026-04-27 13:47:02 +0700

    update config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHSYVsCZAAfcB5j8PbrBFmoQqZ6V33Fso",
-  authDomain: "my-finance-project-id.firebaseapp.com",
-  databaseURL: "https://my-finance-project-id-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "my-finance-project-id",
-  storageBucket: "my-finance-project-id.firebasestorage.app",
-  messagingSenderId: "262444956887",
-  appId: "1:262444956887:web:13c2573d4dbd07a56e0492",
-  measurementId: "G-1MKPP9TVSC"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 // API Key Gemini
-const GEMINI_API_KEY = "AIzaSyBp95rya4v_BFA-qojvinhoDE99RDfWCr8";
+const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY || "";
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
